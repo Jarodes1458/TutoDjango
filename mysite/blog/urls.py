fb0e271492eecb94db78ea4d4b144996da764b3f
Manuel_Jarod <elv-komivi.assm@eduge.ch>
2020-12-18 08:56:30 +0100

    Deuxieme Commit du blog

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -3,4 +3,5 @@ from . import views
 #post_list  est le nom de la vue
 urlpatterns = [
     path('', views.post_list, name='post_list'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
 ]
